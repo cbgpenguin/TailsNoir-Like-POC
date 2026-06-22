8ded56050e21f936b8db4355964672a083cb26a0
cbgpenguin <cbaneygiampoala@gmail.com>
2026-06-22 16:25:56 -0400

    Fixed running after tapping but not holding shift
    again state inheritance is cool

--- RunState.gd
+++ RunState.gd
@@ -5,3 +5,8 @@ extends WalkState
 func enter() -> void:
 	player.animatedSprite2d.play("running (ohh whats the pretty cat boi running from??)")
 	SPEED = 100 # wholy shit I can't belive this works! inherets everything from walk exept uses a modified speed just for running???????!! That's too cool
+
+func update(_delta: float) -> PlayerState:
+	if(!Input.is_action_pressed("run")):
+		return $"../WalkState"
+	return super.update(_delta)
