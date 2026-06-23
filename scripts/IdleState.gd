a488ad6de1d5d794a6a02e70e4a91ab7afba9131
cbgpenguin <cbaneygiampoala@gmail.com>
2026-06-23 11:32:13 -0400

    Idle "stretch" animations
    In IdleState added random timer that can trigger a list or random animations to seamlessly start. For example after a while of idle his foot can do an impatient tap.

--- scripts/IdleState.gd
+++ scripts/IdleState.gd
@@ -1,13 +1,15 @@
 class_name IdleState
 extends PlayerState
 
-#var timeIdle: tim = posgraopuhgraoupharguhope
-#idle animations code
+@onready var stretchTimer: Timer = $stretchTimer
+@onready var stretches: Array = ["foot taps (while idle  looks like regular idle but triggers occasionally after waiting and foot is impatient)"]
 
 func enter() -> void:
 	player.animatedSprite2d.flip_h = false
 	player.animatedSprite2d.play("default (front face short idel)")
 	player.velocity.x = 0;
+	
+	stretchTimer.start(randfn(15, 3.5))
 
 func update(_delta: float) -> PlayerState:
 	if(player.direction && !player.test_move(player.get_global_transform(), Vector2(player.direction, 0.0))): #if the player is trying to move and its not just the kind where you are trying to go up agienst a wall, we should walk
@@ -16,8 +18,13 @@ func update(_delta: float) -> PlayerState:
 		return $"../WalkState"
 
 	#if crouch ect.
-	
 	return self
 	
+func _on_stretch_timer_timeout():
+	await player.animatedSprite2d.animation_looped
+	player.animatedSprite2d.play(stretches[randi() % stretches.size()]) #Chooses random stretch animation from array to start at the bigining of the idle loop so it can look like normal idle loop w/ exta but not be or just smooth transition into
+	await player.animatedSprite2d.animation_finished
+	enter() #reset to normal idle
+
 func exit() -> void:
-	pass
+	stretchTimer.stop()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100.0 * 60 # Pixles/s
 @onready var animatedSprite2d: AnimatedSprite2D = $AnimatedSprite2D
-var colidedLastFrame = true
 var state: PlayerState
 var direction: float
 
@@ -11,14 +9,10 @@ func _ready() -> void:
 	state.enter()
 
 func _physics_process(delta: float) -> void:
+	direction = Input.get_axis("move_left", "move_right")
+
 	var pastState = state
 	state = state.update(delta)
 	if(pastState != state):
 		pastState.exit()
 		state.enter()
-	
-	direction = Input.get_axis("move_left", "move_right")
-
-func setDefaltAnimation():
-	animatedSprite2d.flip_h = false
-	animatedSprite2d.play("default (front face short idel)")
